b114d5017f0e5b7a88ff61deb77e318179c4b802
Arda <ardasa716@gmail.com>
2026-08-08 19:32:21 +0200

    pok

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Clone Flutter SDK if not already present
+if [ ! -d "flutter" ]; then
+  echo "Cloning Flutter SDK (stable)..."
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1 flutter
+fi
+
+# Add Flutter to PATH for the current session
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# Verify Flutter installation
+flutter doctor
+
+# Get dependencies
+flutter pub get
+
+# Build Flutter Web application
+flutter build web --release \
+  --dart-define=SUPABASE_URL=$SUPABASE_URL \
+  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
